126835d0ffbcfd143b06dad32adea61d36603f02
n1rjal <nirjalpaudel54312@gmail.com>
2023-04-12 12:46:39 +0545

    Initialized coding structure

--- core/initializers/router.py
+++ core/initializers/router.py
@@ -1,3 +1,8 @@
+# This is a Python script that serves as a router for a web application. It reads the routes from a
+# YAML file, configures the routes, and maps them to the correct controller. It also allows for custom
+# paths and specific allowed methods to be set without having to specify them in the YAML file.
+
+# Finally, it prints out a table of the paths and their corresponding controllers and allowed methods.
 # router.py is basically the router that handles all the requests and routes them to the correct controller.
 # it configures the routes from routes.yaml and then it uses the controller name to import the controller and
 # call the correct method (get, post, put, delete, etc.)
@@ -13,7 +18,12 @@ paths = {}
 
 CURRENT_DIR = pathlib.Path(os.getcwd())
 
-routes = yaml.load(open(CURRENT_DIR / "routes.yaml", "r"), Loader=yaml.FullLoader)
+# This code block is opening the `routes.yaml` file located in the current directory (`CURRENT_DIR`)
+# in read mode with UTF-8 encoding. It then loads the contents of the file into a Python dictionary
+# using the `yaml.load()` method with the `yaml.FullLoader` loader. The resulting dictionary is stored
+# in the `routes` variable.
+with open(CURRENT_DIR / "routes.yaml", "r", encoding="UTF-8") as route_yaml_file:
+    routes = yaml.load(route_yaml_file, Loader=yaml.FullLoader)
 
 # sets the global root path domain.com/
 if routes.get("root"):
@@ -25,10 +35,18 @@ controller_tweaks = tweaks.get("controller")
 path_tweaks = tweaks.get("path")
 
 for key, value in controller_tweaks.items():
-    # Basically, reads and formats tweaks.controller from routes.yaml
-    if type(value) is str:
+    # This code block checks if the type of `value` is a string. If it is, it adds a new path to the
+    # `paths` dictionary with the key `f"/{value}"` and the value `{"controller_name": key}`. This is
+    # used to create a custom path for a controller without having to specify a path in the
+    # `routes.yaml` file.
+    if isinstance(type(value), str):
         paths.update({f"/{value}": {"controller_name": key}})
-    elif type(value) is dict:
+
+    # This code block is checking if the type of `value` is a dictionary. If it is, it extracts the
+    # `path` and `allowed_methods` values from the dictionary and adds a new path to the `paths`
+    # dictionary with the extracted values. This is used to create a custom path for a controller with
+    # specific allowed methods without having to specify a path in the `routes.yaml` file.
+    elif isinstance(type(value), dict):
         path = value.get("path")
         allowed_methods = value.get("allowed_methods")
         paths.update(
@@ -73,7 +91,13 @@ if resources:
                 f"{LOG_COLOR.WARNING}Please create a directory named '{resource}' in '{CONTROLLERS_ROOT}' directory.{LOG_COLOR.ENDC}"
             )
 
-# print paths as table
+# This code block is printing out a table of the paths and their corresponding controllers and allowed
+# methods. It first prints out a header row with the column names "Path", "Controller", and "Allowed
+# Methods" in the specified color. Then, for each key-value pair in the `paths` dictionary, it prints
+# out a row with the path, controller name, and allowed methods (if any) in a formatted table. The
+# `<20` in the f-strings is used to left-align the text within each column and ensure that each column
+# is 20 characters wide.
+
 print(
     f"{LOG_COLOR.OK_CYAN}{'Path':<20}{'Controller':<20}{'Allowed Methods'}{LOG_COLOR.ENDC}"
 )
